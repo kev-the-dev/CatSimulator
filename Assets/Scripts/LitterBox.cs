2d5810e599ce654913ac19b437ee9c3f5443371e
AlexHagan <alex.h8008@gmail.com>
2019-03-26 13:32:47 -0400

    Added litter scooper button.

---
+++ Assets/Scripts/LitterBox.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LitterBox : MonoBehaviour
+{
+	GameObject poop;
+	Cat catScript;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        poop = GameObject.Find("Poop");
+		catScript = GameObject.Find("Cat").GetComponent<Cat>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	/*
+	void OnMouseDown()
+	{
+		if (catScript.selected_tool == SelectedTool.LITTERBOXSCOOPER)
+		{
+			poop.SetActive(true);
+		}
+
+	}
+	*/
+}
