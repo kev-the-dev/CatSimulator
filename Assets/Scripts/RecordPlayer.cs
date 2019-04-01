f7faade555eaa7f6a83c2be29ec55d134e3885f9
cindynle <cindynle@ufl.edu>
2019-03-31 23:43:42 -0400

    Created scripts, added 3D assets, added particle systems.
    Put Cathy's adoption image in the scene, created a script that makes the cat rotate in place. Added Olivia's 3D models and textures/materials throughout the living room scene. Created and added a record player along with a script that opens up a record player menu on click. Added heart particle effects, dust particle effects, and music note particle effects.

---
+++ Assets/Scripts/RecordPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordPlayer : MonoBehaviour
+{
+    //Script for cilcking on record player and loading a popup screen for changing audio track
+    //Add to Gameobject recordPlayer
+
+    public GameObject recordPlayerScreen;
+ 
+                          
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnMouseOver()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left click.");
+            recordPlayerScreen.SetActive(true);
+        
+        }
+
+
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+           recordPlayerScreen.SetActive(false);
+        }
+
+    }
+
+
+}
