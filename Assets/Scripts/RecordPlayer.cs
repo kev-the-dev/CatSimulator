a7e3ee7f2cfffff25ef340e7e5a7f111674df2ca
AlexHagan <alex.h8008@gmail.com>
2019-04-10 16:05:32 -0500

    Updated record player script to make music note particle effects toggle on/off when turning music on/off

--- Assets/Scripts/RecordPlayer.cs
+++ Assets/Scripts/RecordPlayer.cs
@@ -7,11 +7,13 @@ public class RecordPlayer : MonoBehaviour
     //Script for cilcking on record player to pause/play backgroundMusic
 
     public AudioSource backgroundMusic;
+	GameObject particleEffects;
  
                           
     void Start()
     {
 		backgroundMusic = GameObject.Find("BackgroundMusic").GetComponent<AudioSource>();
+		particleEffects = GameObject.Find("MusicParticles");
     }
 
     // Update is called once per frame
@@ -21,8 +23,10 @@ public class RecordPlayer : MonoBehaviour
         if (!backgroundMusic.isPlaying)
         {
            backgroundMusic.Play();
+		   particleEffects.SetActive(true);
         } else {
 			backgroundMusic.Stop();
+			particleEffects.SetActive(false);
 		}
     }
 }
