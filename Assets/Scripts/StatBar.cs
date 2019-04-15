3a6dba2c5cd366dc32b35528b8394fbdc22d0f21
AlexHagan <alex.h8008@gmail.com>
2019-04-14 21:09:22 -0400

    Changed the S and V (saturation and Value) to make colors lighter

--- Assets/Scripts/StatBar.cs
+++ Assets/Scripts/StatBar.cs
@@ -12,6 +12,6 @@ public class StatBar : MonoBehaviour {
 	public void SetColor()
 	{
 		float hue = slider.value / 3F;
-		fill.color = Color.HSVToRGB(hue, 1F, 1F);
+		fill.color = Color.HSVToRGB(hue, 0.3F, 0.90F);
 	}
 }
