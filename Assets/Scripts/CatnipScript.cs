c15113fa4924244a146195b1f2314cc914f9f6c7
Kevin Allen <kma1660@gmail.com>
2019-04-15 20:08:23 -0400

    Fix catnip/record player tooltip

--- Assets/Scripts/CatnipScript.cs
+++ Assets/Scripts/CatnipScript.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CatnipScript : MonoBehaviour
 {
 	Cat catScript; // Reference to cat class attached to Cat gameobject
+	Text tooltip_text;
 	public const float CATNIP_TIME_DURATION = 60F; // How long catnip effects will last, in seconds.
 	public GameObject UI_Effects;					// Visual effects to call user's attention to catnip
 	
     // Start is called before the first frame update
     void Start()
     {
+		tooltip_text = GameObject.Find("CatnipToolTipText").GetComponent<Text>();
         catScript = GameObject.Find("Cat").GetComponent<Cat>();
 		UI_Effects = GameObject.Find("UIEffects");
-		
     }
-	
+
+	public void OnMouseEnter()
+	{
+		tooltip_text.text = "Feed Catnip";
+	}
+
+	public void OnMouseExit()
+	{
+		tooltip_text.text = "";
+	}
+
 	void OnMouseDown()
 	{
 		Debug.Log("Clicked on catnip.");
